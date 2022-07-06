b93f888883adee0e483e61ee988f814d062e5b00
Nitanta Adhikari <nitanta.adh007@gmail.com>
2022-07-06 20:14:59 +0545

    Dataset add

--- Stationmaster/Module/Main/TabedViewModel.swift
+++ Stationmaster/Module/Main/TabedViewModel.swift
@@ -28,21 +28,15 @@ final class TabedViewModel: ObservableObject {
 //        dataManager.callVehiclePositionsAPI()
 //        downloadManager.startDownload()
         
-        let cleanInterval = TimeInterval(10 * 60)//TimeInterval(24 * 24 * 60 * 60)
+        let cleanInterval = TimeInterval(2 * 60)//TimeInterval(24 * 24 * 60 * 60)
         let cleanupDate = Date().addingTimeInterval(-cleanInterval)
         
         let lastFetchDate = UserDefaults.standard.value(forKey: UserDefaultsKey.datefetchdate) as? Date
         
-        if lastFetchDate == nil, let fetchDate = lastFetchDate, fetchDate <  cleanupDate {
-            isLoading.toggle()
-            adder.addDataset { [weak self] in
-                guard let self = self else { return }
-                self.cleaner.cleanDataset(before: cleanupDate) {
-                    DispatchQueue.main.async {
-                        self.isLoading.toggle()
-                    }
-                }
-            }
+        if let fetchDate = lastFetchDate, fetchDate <  cleanupDate {
+            updateDataset(checkDate: cleanupDate)
+        } else if lastFetchDate == nil {
+            addDataset()
         }
         
     }
@@ -50,6 +44,31 @@ final class TabedViewModel: ObservableObject {
     deinit {
         debugPrint("Deint: \(String(describing: self))")
     }
+    
+    func addDataset() {
+        self.isLoading.toggle()
+        adder.addDataset { [weak self] in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.isLoading.toggle()
+                UserDefaults.standard.set(Date(), forKey: UserDefaultsKey.datefetchdate)
+            }
+        }
+    }
+    
+    func updateDataset(checkDate: Date) {
+        isLoading.toggle()
+        adder.addDataset { [weak self] in
+            guard let self = self else { return }
+            self.cleaner.cleanDataset(before: checkDate) {
+                DispatchQueue.main.async {
+                    self.isLoading.toggle()
+                    UserDefaults.standard.set(Date(), forKey: UserDefaultsKey.datefetchdate)
+                }
+            }
+        }
+    }
+
 }
 
 extension TabedViewModel {
